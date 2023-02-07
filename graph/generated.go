c2a067d2b04639d22e3cd60a22aa62d8d408ac66
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-02-07 17:47:14 +0900

    :ambulance: ロジック記述

--- graph/generated.go
+++ graph/generated.go
@@ -62,6 +62,7 @@ type ComplexityRoot struct {
 
 	Query struct {
 		Comments func(childComplexity int) int
+		Item     func(childComplexity int, id string) int
 		Items    func(childComplexity int) int
 		Users    func(childComplexity int) int
 	}
@@ -72,6 +73,7 @@ type ComplexityRoot struct {
 		Good        func(childComplexity int) int
 		ID          func(childComplexity int) int
 		Image       func(childComplexity int) int
+		Prise       func(childComplexity int) int
 		Title       func(childComplexity int) int
 		User        func(childComplexity int) int
 		UserID      func(childComplexity int) int
@@ -93,6 +95,7 @@ type MutationResolver interface {
 }
 type QueryResolver interface {
 	Items(ctx context.Context) ([]*model.ShopItem, error)
+	Item(ctx context.Context, id string) (*model.ShopItem, error)
 	Comments(ctx context.Context) ([]*model.Comment, error)
 	Users(ctx context.Context) ([]*model.User, error)
 }
@@ -189,6 +192,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 
 		return e.complexity.Query.Comments(childComplexity), true
 
+	case "Query.item":
+		if e.complexity.Query.Item == nil {
+			break
+		}
+
+		args, err := ec.field_Query_item_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Query.Item(childComplexity, args["id"].(string)), true
+
 	case "Query.items":
 		if e.complexity.Query.Items == nil {
 			break
@@ -237,6 +252,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.ShopItem.Image(childComplexity), true
+
+	case "ShopItem.prise":
+		if e.complexity.ShopItem.Prise == nil {
+			break
+		}
+
+		return e.complexity.ShopItem.Prise(childComplexity), true
 
 	case "ShopItem.title":
 		if e.complexity.ShopItem.Title == nil {
@@ -441,6 +463,21 @@ func (ec *executionContext) field_Query___type_args(ctx context.Context, rawArgs
 		}
 	}
 	args["name"] = arg0
+	return args, nil
+}
+
+func (ec *executionContext) field_Query_item_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 string
+	if tmp, ok := rawArgs["id"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("id"))
+		arg0, err = ec.unmarshalNID2string(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["id"] = arg0
 	return args, nil
 }
 
@@ -707,6 +744,8 @@ func (ec *executionContext) fieldContext_Mutation_createShopItem(ctx context.Con
 				return ec.fieldContext_ShopItem_image(ctx, field)
 			case "good":
 				return ec.fieldContext_ShopItem_good(ctx, field)
+			case "prise":
+				return ec.fieldContext_ShopItem_prise(ctx, field)
 			case "userID":
 				return ec.fieldContext_ShopItem_userID(ctx, field)
 			case "user":
@@ -909,6 +948,8 @@ func (ec *executionContext) fieldContext_Query_items(ctx context.Context, field 
 				return ec.fieldContext_ShopItem_image(ctx, field)
 			case "good":
 				return ec.fieldContext_ShopItem_good(ctx, field)
+			case "prise":
+				return ec.fieldContext_ShopItem_prise(ctx, field)
 			case "userID":
 				return ec.fieldContext_ShopItem_userID(ctx, field)
 			case "user":
@@ -918,6 +959,78 @@ func (ec *executionContext) fieldContext_Query_items(ctx context.Context, field 
 			}
 			return nil, fmt.Errorf("no field named %q was found under type ShopItem", field.Name)
 		},
+	}
+	return fc, nil
+}
+
+func (ec *executionContext) _Query_item(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_Query_item(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Query().Item(rctx, fc.Args["id"].(string))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		return graphql.Null
+	}
+	res := resTmp.(*model.ShopItem)
+	fc.Result = res
+	return ec.marshalOShopItem2ᚖgithubᚗcomᚋryujiᚑcre8iveᚋhuyouᚑserverᚋgraphᚋmodelᚐShopItem(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_Query_item(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Query",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			switch field.Name {
+			case "id":
+				return ec.fieldContext_ShopItem_id(ctx, field)
+			case "title":
+				return ec.fieldContext_ShopItem_title(ctx, field)
+			case "description":
+				return ec.fieldContext_ShopItem_description(ctx, field)
+			case "image":
+				return ec.fieldContext_ShopItem_image(ctx, field)
+			case "good":
+				return ec.fieldContext_ShopItem_good(ctx, field)
+			case "prise":
+				return ec.fieldContext_ShopItem_prise(ctx, field)
+			case "userID":
+				return ec.fieldContext_ShopItem_userID(ctx, field)
+			case "user":
+				return ec.fieldContext_ShopItem_user(ctx, field)
+			case "comments":
+				return ec.fieldContext_ShopItem_comments(ctx, field)
+			}
+			return nil, fmt.Errorf("no field named %q was found under type ShopItem", field.Name)
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Query_item_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return
 	}
 	return fc, nil
 }
@@ -1372,6 +1485,50 @@ func (ec *executionContext) fieldContext_ShopItem_good(ctx context.Context, fiel
 	return fc, nil
 }
 
+func (ec *executionContext) _ShopItem_prise(ctx context.Context, field graphql.CollectedField, obj *model.ShopItem) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_ShopItem_prise(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return obj.Prise, nil
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(int)
+	fc.Result = res
+	return ec.marshalNInt2int(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_ShopItem_prise(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "ShopItem",
+		Field:      field,
+		IsMethod:   false,
+		IsResolver: false,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			return nil, errors.New("field of type Int does not have child fields")
+		},
+	}
+	return fc, nil
+}
+
 func (ec *executionContext) _ShopItem_userID(ctx context.Context, field graphql.CollectedField, obj *model.ShopItem) (ret graphql.Marshaler) {
 	fc, err := ec.fieldContext_ShopItem_userID(ctx, field)
 	if err != nil {
@@ -1739,6 +1896,8 @@ func (ec *executionContext) fieldContext_User_ShopItem(ctx context.Context, fiel
 				return ec.fieldContext_ShopItem_image(ctx, field)
 			case "good":
 				return ec.fieldContext_ShopItem_good(ctx, field)
+			case "prise":
+				return ec.fieldContext_ShopItem_prise(ctx, field)
 			case "userID":
 				return ec.fieldContext_ShopItem_userID(ctx, field)
 			case "user":
@@ -3584,7 +3743,7 @@ func (ec *executionContext) unmarshalInputNewShopItem(ctx context.Context, obj i
 		asMap[k] = v
 	}
 
-	fieldsInOrder := [...]string{"id", "title", "description", "image", "good"}
+	fieldsInOrder := [...]string{"id", "title", "description", "image", "good", "prise"}
 	for _, k := range fieldsInOrder {
 		v, ok := asMap[k]
 		if !ok {
@@ -3628,6 +3787,14 @@ func (ec *executionContext) unmarshalInputNewShopItem(ctx context.Context, obj i
 
 			ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("good"))
 			it.Good, err = ec.unmarshalOInt2ᚖint(ctx, v)
+			if err != nil {
+				return it, err
+			}
+		case "prise":
+			var err error
+
+			ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("prise"))
+			it.Prise, err = ec.unmarshalNInt2int(ctx, v)
 			if err != nil {
 				return it, err
 			}
@@ -3842,6 +4009,26 @@ func (ec *executionContext) _Query(ctx context.Context, sel ast.SelectionSet) gr
 			out.Concurrently(i, func() graphql.Marshaler {
 				return rrm(innerCtx)
 			})
+		case "item":
+			field := field
+
+			innerFunc := func(ctx context.Context) (res graphql.Marshaler) {
+				defer func() {
+					if r := recover(); r != nil {
+						ec.Error(ctx, ec.Recover(ctx, r))
+					}
+				}()
+				res = ec._Query_item(ctx, field)
+				return res
+			}
+
+			rrm := func(ctx context.Context) graphql.Marshaler {
+				return ec.OperationContext.RootResolverMiddleware(ctx, innerFunc)
+			}
+
+			out.Concurrently(i, func() graphql.Marshaler {
+				return rrm(innerCtx)
+			})
 		case "comments":
 			field := field
 
@@ -3943,6 +4130,13 @@ func (ec *executionContext) _ShopItem(ctx context.Context, sel ast.SelectionSet,
 		case "good":
 
 			out.Values[i] = ec._ShopItem_good(ctx, field, obj)
+
+			if out.Values[i] == graphql.Null {
+				atomic.AddUint32(&invalids, 1)
+			}
+		case "prise":
+
+			out.Values[i] = ec._ShopItem_prise(ctx, field, obj)
 
 			if out.Values[i] == graphql.Null {
 				atomic.AddUint32(&invalids, 1)
